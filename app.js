6f7aca58bb78c10377fca8330a88797e7b7a9f45
irsyahms <irsyahmardiahs@gmail.com>
2018-11-19 13:52:29 +0700

    add comment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+// adfkadjajsdkj
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
